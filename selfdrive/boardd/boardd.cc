9b9a1309f85b212810c8b43a1e059ff7de79eea5
hgnme <44087247+hgnme@users.noreply.github.com>
2020-11-27 22:27:59 +1100

    Include latest vw changes from community-master-merge
    - disengage on gas
    - latest vw interface files

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -108,8 +108,9 @@ void safety_setter_thread() {
   capnp::FlatArrayMessageReader cmsg(amsg);
   cereal::CarParams::Reader car_params = cmsg.getRoot<cereal::CarParams>();
   cereal::CarParams::SafetyModel safety_model = car_params.getSafetyModel();
-
-  panda->set_unsafe_mode(0);  // see safety_declarations.h for allowed values
+  
+  LOGW("setting unsafe mode: disable enforcement of disengage on gas");
+  panda->set_unsafe_mode();
 
   auto safety_param = car_params.getSafetyParam();
   LOGW("setting safety model: %d with param %d", (int)safety_model, safety_param);
